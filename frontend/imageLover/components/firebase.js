357ab13a7390ca628e39dd1622e8c57ea173c3b6
maria6688 <121517781+mariazhou668899@users.noreply.github.com>
2024-08-04 09:01:45 -0700

    Update firebase.js

--- frontend/imageLover/components/firebase.js
+++ frontend/imageLover/components/firebase.js
@@ -1,25 +1,7 @@
 // firebaseConfig.js
 import { initializeApp, getApp  } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence, getAuth} from 'firebase/auth';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "XXXXXXXXXXXXXXXXXXXXXXX",
-    authDomain: "XXXXXXXXXXXXXXXXXXXXXXX",
-    projectId: "XXXXXXXXXXXXXXXXXXXXXXX",
-    storageBucket: "XXXXXXXXXXXXXXXXXXXXXXX",
-    messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXX",
-    appId: "XXXXXXXXXXXXXXXXXXXXXXX"
-};
+..............................................
 
-const app = initializeApp(firebaseConfig);
-
-// Initialize firestore
-const db = getFirestore(app);
-
-const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-  });
 
   export { app, auth, getApp, getAuth, db};
