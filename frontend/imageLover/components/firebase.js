3d9362ca117aa367849fc7385d3b2b9c707cfbc7
maria6688 <121517781+mariazhou668899@users.noreply.github.com>
2024-08-04 09:02:59 -0700

    Update firebase.js

--- frontend/imageLover/components/firebase.js
+++ frontend/imageLover/components/firebase.js
@@ -1,7 +1,8 @@
 // firebaseConfig.js
 import { initializeApp, getApp  } from 'firebase/app';
 
-..............................................
+//..............................................
+// Delete to avoid copying
 
 
   export { app, auth, getApp, getAuth, db};
