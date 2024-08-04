62f4edd41697fec7a6e74935a1252d3c85b1d3d8
maria6688 <121517781+mariazhou668899@users.noreply.github.com>
2024-08-04 09:03:57 -0700

    Update index.js

--- backend/OpenAI-Services/index.js
+++ backend/OpenAI-Services/index.js
@@ -7,25 +7,9 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-const limiter = rateLimit({
-    windowMS: 10 * 60 * 1000, // 10 minutes
-    max: 90
-});
+//..............................................
+// Delete to avoid copying
+//..............................................
 
-app.use(limiter);
-app.set('trust proxy', 1);
-
-// Increase payload size limit to 50 megabytes
-app.use(express.json({ limit: '50mb' }));
-
-// Enable cors
-app.use(cors());
-
-// Routes
-app.use('/openai', require('./routes/openai.js'));
-
-app.get('/', async (req, res) => {
-    res.status(200).json("OpenAI Proxy Service is running");
-});
 
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
